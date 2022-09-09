1a51af6f60a9bf8e78830b056a05ecf64f17301b
Michael Graf <michael.graf3110@gmail.com>
2022-09-09 12:41:14 +0200

    train command and args display and rebuild button

--- components/domains/local-train/LocalTrainRun.vue
+++ components/domains/local-train/LocalTrainRun.vue
@@ -17,6 +17,15 @@ export default Vue.extend({
             type: String,
         },
     },
+    data() {
+        return {
+            busy: false,
+            commandEdit: false,
+            command: '',
+            commandArgsEdit: '',
+            commandArgs: '',
+        };
+    },
     methods: {
         async run() {
             console.log('Run', this.datasetId, this.configId);
@@ -33,17 +42,84 @@ export default Vue.extend({
 
 <template>
     <div
-        class="d-flex flex-row justify-content-end"
+        class="d-flex flex-row justify-content-between mb-4"
     >
-        <button
-            class="btn btn-primary"
-            @click="run"
+        <div
+            class="d-flex flex-column align-items-center"
         >
-            <span class="icon">
-                <i class="fas fa-play" />
-            </span>
-            <span>Run</span>
-        </button>
+            <h6>
+                Command
+                <button
+                    class="btn btn-sm btn"
+                    @click="commandEdit = !commandEdit"
+                >
+                    <i class="fas fa-edit" />
+                </button>
+            </h6>
+            <div
+                v-if="commandEdit"
+            >
+                <input
+                    v-model="command"
+                    type="text"
+                    class="form-control"
+                    :placeholder="train.command"
+                >
+            </div>
+            <div
+                v-else
+            >
+                <span>{{ train.command }}</span>
+            </div>
+        </div>
+        <div
+            class="d-flex flex-column align-items-center"
+        >
+            <h6>
+                Command Arguments
+                <button
+                    class="btn btn-sm btn"
+                    @click="commandArgsEdit = !commandArgsEdit"
+                >
+                    <i class="fas fa-edit" />
+                </button>
+            </h6>
+            <div
+                v-if="commandArgsEdit"
+            >
+                <input
+                    v-model="commandArgs"
+                    type="text"
+                    class="form-control"
+                    :placeholder="train.command_args"
+                >
+            </div>
+            <div
+                v-else
+            >
+                <span>{{ train.command_args }}</span>
+            </div>
+        </div>
+        <div class="d-flex flex-row justify-content-end">
+            <button
+                class="btn btn-warning mr-2"
+                @click="run"
+            >
+                <span class="icon">
+                    <i class="fas fa-tools" />
+                </span>
+                <span>Rebuild</span>
+            </button>
+            <button
+                class="btn btn-primary"
+                @click="run"
+            >
+                <span class="icon">
+                    <i class="fas fa-play" />
+                </span>
+                <span>Run</span>
+            </button>
+        </div>
     </div>
 </template>
 
