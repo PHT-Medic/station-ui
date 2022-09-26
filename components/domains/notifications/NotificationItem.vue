11af63abedfff1f0eed170b9a03e679f2abdc9af
Michael Graf <michael.graf3110@gmail.com>
2022-09-26 15:22:54 +0200

    notifications dropdown

---
+++ components/domains/notifications/NotificationItem.vue
@@ -0,0 +1,67 @@
+<script lang="ts">
+import { PropType } from 'vue';
+
+export default {
+    name: 'NotificationItem',
+    props: {
+        item: {
+            type: Object as PropType<Notification>,
+            required: true,
+        },
+    },
+    methods: {
+        handleDelete() {
+            this.$emit('delete', this.item.id);
+        },
+        handleRead() {
+            this.$emit('read', this.item.id);
+        },
+    },
+};
+
+</script>
+
+<template>
+    <div class="container-fluid border border-bottom-0">
+        <div class="col container-fluid">
+            <div class="row">
+                <nuxt-link
+                    :to="'/notifications/' + item.id"
+                >
+                    <h6
+                        class="mt-1 ml-1"
+                    >
+                        {{ item.title }}
+                    </h6>
+                </nuxt-link>
+            </div>
+            <div class="row">
+                <div class="col">
+                    <timeago
+                        :datetime="item.created_at"
+                        class="ml-1 font-weight-bold"
+                        style="font-size: 1rem"
+                    />
+                </div>
+                <div class="col">
+                    <button
+                        class="btn btn-info btn-sm"
+                        @click="handleRead"
+                    >
+                        <i class="fas fa-check"></i>
+                    </button>
+                    <button
+                        class="btn btn-danger btn-sm"
+                        @click="handleDelete"
+                    >
+                        <i class="fas fa-trash-alt"></i>
+                    </button>
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
+
+<style scoped>
+
+</style>
--- layouts/header.vue
+++ layouts/header.vue
@@ -5,7 +5,12 @@
   view the LICENSE file that was distributed with this source code.
   -->
 <script lang="ts">
+import NotificationList from "../components/domains/notifications/NotificationList.vue";
+
 export default {
+    components: {
+        NotificationList,
+    },
     async asyncData(ctx) {
         const notifications = await ctx.$stationApi.notification.getNotifications();
         return {
@@ -46,6 +51,10 @@ export default {
         this.timer = setInterval(this.getNotifications, 10000);
     },
     destroyed() {
+        clearInterval(this.timer);
+    },
+
+    beforeDestroy() {
         clearInterval(this.timer);
     },
     methods: {
@@ -106,8 +115,7 @@ export default {
                         <li class="nav-item">
                             <div class="dropdown">
                                 <div
-                                    class="nav-link dropdown-toggle"
-                                    data-toggle="dropdown"
+                                    class="nav-link"
                                 >
                                     <div
                                         class="notification-wrapper"
@@ -124,8 +132,8 @@ export default {
                                         </span>
                                     </div>
                                 </div>
-                                <div class="dropdown-menu">
-                                    Dropdown
+                                <div class="dropdown-content">
+                                    <notification-list :items="notifications" />
                                 </div>
                             </div>
                         </li>
@@ -170,5 +178,19 @@ export default {
     font-weight: bold;
     margin: -10px 10px 0;
 }
+
+.dropdown-content {
+    display: none;
+    position: absolute;
+    background-color: #f1f1f1;
+    min-width: 30rem;
+    max-height: 20rem;
+    box-shadow: 0px 8px 16px 0px rgba(0,0,0,0.2);
+    z-index: 1;
+    left: -25rem;
+    overflow-y: scroll;
+}
+
+.dropdown:hover .dropdown-content {display: flex;}
 
 </style>
